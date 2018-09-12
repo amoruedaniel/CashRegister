cf902763e0ea4ac6b1fc42237c60349c2b8fdf36
Paul Steimel <paulsteimel@gmail.com>
2018-09-12 16:21:40 -0500

    Initial Commit

---
+++ CashRegister.js
@@ -0,0 +1,25 @@
+function checkCashRegister(price, cash, cid) {
+    const REGISTER_STATUS = {
+        registerOpen: 'OPEN',
+        registerClosed: 'CLOSED',
+        registerInsufficient: 'INSUFICIENT_FUNDS'
+    }
+    
+    var change;
+
+    // Here is your change, ma'am.
+    return change;
+  }
+  
+  // Example cash-in-drawer array:
+  // [["PENNY", 1.01],
+  // ["NICKEL", 2.05],
+  // ["DIME", 3.1],
+  // ["QUARTER", 4.25],
+  // ["ONE", 90],
+  // ["FIVE", 55],
+  // ["TEN", 20],
+  // ["TWENTY", 60],
+  // ["ONE HUNDRED", 100]]
+  
+  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
