d30ea5685ad638f57a4098e1681a58c43892844d
Paul Steimel <paulsteimel@gmail.com>
2018-09-13 09:48:36 -0500

    Fixed typo in insufficient

--- CashRegister.js
+++ CashRegister.js
@@ -1,8 +1,8 @@
 function checkCashRegister(price, cash, cid) {
     const REGISTER_STATUS = {
-        registerOpen: 'OPEN',
-        registerClosed: 'CLOSED',
-        registerInsufficient: 'INSUFICIENT_FUNDS'
+        registerOpen: "OPEN",
+        registerClosed: "CLOSED",
+        registerInsufficient: "INSUFFICIENT_FUNDS"
     }
     
     let cashRegister = {status: '', change: cid};
@@ -11,12 +11,15 @@ function checkCashRegister(price, cash, cid) {
     const cashAddition = (a , b) => a + b[1];
     let cashInDrawer = cid.reduce(cashAddition, 0).toFixed(2);
     
-    console.log(cashInDrawer);
+    console.log(changeDue);
+    console.log(cashInDrawer)
 
     if (changeDue > cashInDrawer){
         cashRegister.status = REGISTER_STATUS.registerInsufficient;
         cashRegister.change = [];
     }
+
+    console.log(cashRegister);
 
     // Here is your change, ma'am.
     return cashRegister;
@@ -33,4 +36,4 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
