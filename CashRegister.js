6dbe685c8b4a2eebc8be110f88c23f0c13ee142a
Paul Steimel <paulsteimel@gmail.com>
2018-09-13 10:14:19 -0500

    If statement for CLOSED status condition

--- CashRegister.js
+++ CashRegister.js
@@ -6,7 +6,7 @@ function checkCashRegister(price, cash, cid) {
     }
     
     let cashRegister = {status: '', change: cid};
-    let changeDue = cash - price;
+    let changeDue = (cash - price).toFixed(2);
     
     const cashAddition = (a , b) => a + b[1];
     let cashInDrawer = cid.reduce(cashAddition, 0).toFixed(2);
@@ -14,26 +14,21 @@ function checkCashRegister(price, cash, cid) {
     console.log(changeDue);
     console.log(cashInDrawer)
 
+    if (changeDue === cashInDrawer){
+        cashRegister.status = REGISTER_STATUS.registerClosed;
+        cashRegister.change = cid;
+    }
+
     if (changeDue > cashInDrawer){
         cashRegister.status = REGISTER_STATUS.registerInsufficient;
         cashRegister.change = [];
     }
 
-    console.log(cashRegister);
 
-    // Here is your change, ma'am.
+    console.log(cashRegister);
     return cashRegister;
   }
   
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
+
   
-  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
