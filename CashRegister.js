c5a93b518ea810d40af9400e9ac0fc7ff66d869d
Paul Steimel <paulsteimel@gmail.com>
2018-09-13 15:48:12 -0500

    All tests pass

--- CashRegister.js
+++ CashRegister.js
@@ -6,23 +6,67 @@ function checkCashRegister(price, cash, cid) {
     }
     
     let cashRegister = {status: '', change: cid};
-    let changeDue = (cash - price).toFixed(2);
+    let changeDue = parseFloat(cash - price).toFixed(2);
     
     const cashAddition = (a , b) => a + b[1];
     let cashInDrawer = cid.reduce(cashAddition, 0).toFixed(2);
-    
-    console.log(changeDue);
-    console.log(cashInDrawer)
 
-    if (changeDue === cashInDrawer){
+    if (Number(changeDue) === Number(cashInDrawer)){
         cashRegister.status = REGISTER_STATUS.registerClosed;
         cashRegister.change = cid;
-    }
-
-    if (changeDue > cashInDrawer){
+    }else if (Number(changeDue) > Number(cashInDrawer)){
         cashRegister.status = REGISTER_STATUS.registerInsufficient;
         cashRegister.change = [];
+    }else {
+        cashRegister.status = REGISTER_STATUS.registerOpen;
+        cashRegister.change = calculateCustomerChange(changeDue, cid);
+
+
     }
+
+    function calculateCustomerChange(changeDue, currencyInDrawer){
+        const changeArray = [];
+        const currencyDictionary = {
+            "PENNY"         :  0.01,
+            "NICKEL"        :  0.05,
+            "DIME"          :  0.10,
+            "QUARTER"       :  0.25,
+            "ONE"           :  1.00,
+            "FIVE"          :  5.00,
+            "TEN"           : 10.00,
+            "TWENTY"        : 20.00,
+            "ONE HUNDRED"   : 100.00
+        };
+
+        for (let i = currencyInDrawer.length -1; i >= 0; i-- ){
+            const currencyName = currencyInDrawer[i][0];
+            const currencyValue = currencyDictionary[currencyName];
+            const currenyTotal = currencyInDrawer[i][1];
+            let currencyAmmount = (currenyTotal / currencyValue).toFixed(2);
+            let currencyToReturn = 0;
+
+            while (changeDue >= currencyValue && currencyAmmount > 0){
+                changeDue -= currencyValue;
+                changeDue = changeDue.toFixed(2);
+                currencyAmmount --;
+                currencyToReturn++;
+            }
+
+            if (currencyToReturn > 0){
+                changeArray.push([currencyName, currencyToReturn * currencyValue]);
+            }
+            
+            if (changeDue == 0.00){
+                return changeArray;
+            }else if (i === 0){
+                cashRegister.status = REGISTER_STATUS.registerInsufficient;
+                return [];
+            }else{
+
+            }
+        }
+    }
+
 
 
     console.log(cashRegister);
@@ -31,4 +75,4 @@ function checkCashRegister(price, cash, cid) {
   
 
   
-  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
